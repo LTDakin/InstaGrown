fd243f4875b28aa715676645ed738f9365605b1a
neng1210 <neng1210@email.arizona.edu>
2021-08-05 16:42:34 -0700

    server.js comments

--- InstaGrown/server.js
+++ InstaGrown/server.js
@@ -1,16 +1,18 @@
 /*
   Author: Lloyd Dakin, Nicholas Eng
   Class: CSC337
-  Description: server for the InstaGrown app, uses mongoose to store information like users, post friends statuses etc.
+  Description: server for the InstaGrown app,
+  uses mongoose to store information like users, post friends statuses etc.
 */
 
 /*    SETUP   */
-//imports
+//imports reuqired modules
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const crypto = require('crypto');
 
+// sets up port, modules
 const app = express();
 const port = 3000;
 app.use(express.urlencoded({ extended: true }));
@@ -26,7 +28,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 /*  SCHEMAS  */
 var Schema = mongoose.Schema;
 
-//User schema
+// Defines User schema
 var UserSchema = new Schema({
     Username: String,
     Salt: String,
@@ -34,42 +36,28 @@ var UserSchema = new Schema({
     Bio: String,
     Email: String,
     Friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    //Posts: [{ type: Schema.Types.ObjectID, ref: 'Post' }]
     Friends: [],
     Posts: []
 });
 var Users = mongoose.model('Users', UserSchema);
 
-//Post Schema
+// Defines Post Schema
 var PostSchema = new Schema({
-    //Poster: UserSchema, // maybe uncomment later?
     Title: String,
     Content: String,
     Image: String, //TODO how to implement images
     Comments: [],
     Likes: [],
-    //Comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-    //Likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 });
 var Posts = mongoose.model('Posts', PostSchema);
 
-//Comment Schema
+// Defines Comment Schema
 var CommentSchema = new Schema({
-    //Poster: UserSchema,
     Content: String,
     Likes: []
-    //Likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 });
 var Comment = mongoose.model('Comment', CommentSchema);
 
-//Message Schema
-var MessageSchema = new Schema({
-    Sender: UserSchema,
-    Receiver: UserSchema,
-    Content: String,
-    Time: Number
-});
-var Message = mongoose.model('Message', MessageSchema);
 
 /*    REQUESTS   */
 
@@ -101,7 +89,6 @@ app.post("/login/user/", (req, res) => {
             } else {
                 //add cookie for login 10 min timer
                 res.cookie("login", { username: user }, { maxAge: 900000 });
-                console.log("login successful!")
                 res.end(JSON.stringify({ text: 'ok' }));
             }
           });
@@ -124,32 +111,31 @@ app.post("/add/user/", (req, res) => {
     Users.find({ Username: user }).exec(function(error, results) {
         //if no other accounts with that name exist
         if (results.length == 0) {
-          //generate random string for salt, iterations
-          var salt = crypto.randomBytes(64).toString('base64');
-          var iterations = 100;
-          //hash the salt and password using crypto
-          crypto.pbkdf2(pass, salt, iterations, 64, 'sha512', (err, hash) => {
+            //generate random string for salt, iterations
+            var salt = crypto.randomBytes(64).toString('base64');
+            var iterations = 100;
+            //hash the salt and password using crypto
+            crypto.pbkdf2(pass, salt, iterations, 64, 'sha512', (err, hash) => {
             if(err) throw err;
             var hStr = hash.toString('base64');
 
             //create and save the user
             var newUser = new Users({
-              Username: user,
-              Salt: salt,
-              Hash: hStr,
-              Bio: bio,
-              Email: email,
-              Friends: [],
-              Posts: []
+                Username: user,
+                Salt: salt,
+                Hash: hStr,
+                Bio: bio,
+                Email: email,
+                Friends: [],
+                Posts: []
             });
+            // saves new user to db
             newUser.save(function(err) { if (err) console.log("error occured saving to db"); });
-            console.log('user created!');
             res.end(JSON.stringify({ text: 'User created!' }));
           });
         }
         //user exists, send error
         else {
-            console.log('user already exists!');
             res.end(JSON.stringify({ text: 'error' }));
         }
     });
@@ -169,6 +155,7 @@ app.get("/search/user/:KEY", (req, res) => {
             //add user to list of found users
             result.push(results[i]);
         }
+        // sends the results back to the client side
         res.end(JSON.stringify(result, null, 2));
     });
 });
@@ -184,39 +171,30 @@ app.get("/search/posts/:KEY", (req, res) => {
             //add post to list of found posts
             result.push(results[i]);
         }
+        // sends the results back to the client side
         res.end(JSON.stringify(result, null, 2));
-    });
-});
-
-
-// for testing, not called in functions
-app.get("/home.html/get/posts", (req, res) => {
-    Posts.find({}).exec(function(error, results) {
-        res.end(JSON.stringify(results, null, 4));
-    });
-});
-// for testing, not called in functions
-app.get("/home.html/get/users", (req, res) => {
-    Users.find({}).exec(function(error, results) {
-        res.end(JSON.stringify(results, null, 4));
     });
 });
 
 //adds a comment to a post
 app.get("/comment/post/:TITLE/:CONTENT/:COMMENT", (req, res) => {
+  // gets comment info
   let t = req.params.TITLE;
   let c = req.params.CONTENT;
   let commentParam = req.params.COMMENT;
   userN = req.cookies.login.username;
 
-  // save
+  // finds post
   Posts.find({Title:t, Content: c}).exec(function(error, results) {
     if (results.length != 0) {
+      // creates comment + saves it
       var newComment = new Comment({Content: commentParam, Likes:[]});
       newComment.save(function (err) { if (err) console.log("ERROR");});
+      // updates Comments array posts with new comment
       db.collection("posts").update(
         { Title:t, Content: c },
         { $push: { Comments: newComment } });
+      // calls updateComments to update User's array of Posts
       updateComments(t, c, userN, newComment);
       res.send("GOOD");
     } else {
@@ -226,13 +204,20 @@ app.get("/comment/post/:TITLE/:CONTENT/:COMMENT", (req, res) => {
 });
 
 // used to update the array of posts in user when a post is liked
+// needs to be called because User's list of Posts will not update
+// because objects not passed by reference
 function updateComments(t, c, u){
   userN = u;
   updated = [];
+
+  // gets user
   Users.find({Username:userN}).exec(function(error, results) {
     var user = results[0];
+    // gets posts
     Posts.find({Title:t, Content: c}).exec(function(error, results) {
       newPost = results[0];
+
+      // adds each post in posts array
       for (i in user.Posts) {
         if (user.Posts[i].Title == t && user.Posts[i].Content == c) {
           updated.push(newPost);
@@ -240,6 +225,9 @@ function updateComments(t, c, u){
           updated.push(user.Posts[i]);
         }
       }
+
+      // updates users' Posts array
+      // user's Posts array now has the Comment in the Post
       db.collection("users").update(
         {Username: userN},
         {$set: {Posts: updated }}
@@ -254,9 +242,9 @@ app.get("/get/user/bio", (req, res) => {
   //look up the user and if exists, return the bio
   Users.find({Username: user}).exec(function(err, results){
     if(results.length == 0){
-      console.log("username from cookies " + user + " not found in database");
       res.end(JSON.stringify({ text: 'error' }));
     } else {
+      // if found, sends info
       userData = results[0];
       res.end(JSON.stringify({ bio: userData.Bio }));
     }
@@ -270,9 +258,7 @@ app.get('/get/user/friends', (req, res) => {
 
     //Search for user to get their friends data
     Users.find({ Username: user }).populate("Friends").exec(function(error, results) {
-    //Users.find({Username:user}).exec(function(error, results) {
         if (results.length == 0) {
-            console.log("username from cookies " + user + " not found in database");
             res.end(JSON.stringify({ text: 'error' }));
         } else {
             userData = results[0];
@@ -324,11 +310,15 @@ app.post("/add/user/friend", (req, res) => {
 
 //adds a like to a post
 app.get("/like/post/:TITLE/:CONTENT", (req, res) => {
+  // gets post info
   let t = req.params.TITLE;
   let c = req.params.CONTENT;
   userN = req.cookies.login.username;
+
+  // finds post given parameters
   Posts.find({Title:t, Content: c}).exec(function(error, results) {
     if (results.length != 0) {
+      // if already liked, cant like again
       if (results[0].Likes.includes(userN)) {
         res.send("You Cannot Like a Post More than Once!");
       } else {
@@ -337,6 +327,7 @@ app.get("/like/post/:TITLE/:CONTENT", (req, res) => {
           {Title:t, Content: c},
           {$push: {Likes: userN }}
         );
+        // updates posts
         updatePosts(t, c, userN);
         res.send("GOOD");
       }
@@ -347,13 +338,17 @@ app.get("/like/post/:TITLE/:CONTENT", (req, res) => {
 });
 
 // used to update the array of posts in user when a post is liked
+// so update can be seen in the Users Post array
 function updatePosts(t, c, u){
   userN = u;
   updated = [];
+  // finds user with username
   Users.find({Username:userN}).exec(function(error, results) {
     var user = results[0];
+    // gets posts
     Posts.find({Title:t, Content: c}).exec(function(error, results) {
       newPost = results[0];
+      // adds posts to array
       for (i in user.Posts) {
         if (user.Posts[i].Title == t && user.Posts[i].Content == c) {
           updated.push(newPost);
@@ -361,6 +356,7 @@ function updatePosts(t, c, u){
           updated.push(user.Posts[i]);
         }
       }
+      // updates users "Posts" array
       db.collection("users").update(
         {Username: userN},
         {$set: {Posts: updated }}
@@ -369,17 +365,12 @@ function updatePosts(t, c, u){
   });
 }
 
-
-//adds a like to a comment
-app.post("/like/comment", (req, res) => {
-
-});
-
 //shares another user's post as a new post from the current user
 app.get("/share/post/:T/:C", (req, res) => {
   userN = req.cookies.login.username;
   title ="Sharing " + "'"+req.params.T+"'";
   content = req.params.C;
+  // finds user
   Users.find({ Username: userN }).exec(function(error, results) {
       if (results.length == 1) {
           // creates and saves post
@@ -405,26 +396,18 @@ app.get("/share/post/:T/:C", (req, res) => {
 
 //gets the posts from the server
 app.get("/get/posts", (req, res) => {
+  // gets posts
   Posts.find({}).exec(function(error, results) {
+    // reverses so newest posts appear at top
     results.reverse();
     res.end(JSON.stringify(results, null, 4));
   });
-  /*
-    userN = req.cookies.login.username;
-    // searches for username
-    Users.find({ Username: userN }).exec(function(error, results) {
-        if (results.length == 1) {
-            postsList = results[0].Posts;
-            res.end(JSON.stringify(postsList, null, 4));
-            // if no username matches, send no such username
-        } else {
-            res.end("BAD");
-        }
-    });*/
 });
 
+// creates a post and adds it to the db
 app.post("/create/post", (req, res) => {
     userN = req.cookies.login.username;
+    // gets username
     Users.find({ Username: userN }).exec(function(error, results) {
         if (results.length == 1) {
             // creates and saves post
@@ -457,7 +440,6 @@ app.post("/update/bio", (req, res) => {
     //Search for user to update their bio
     Users.find({ Username: user }).exec(function(error, results) {
         if (results.length == 0) {
-            console.log("username from cookies " + user + " not found in database");
             res.end(JSON.stringify({ text: 'error' }));
         } else {
             //update the bio and save back into DB
@@ -470,13 +452,6 @@ app.post("/update/bio", (req, res) => {
     });
 });
 
-//chats.html requests---------------------------------------------------------------------
-
-//sends a specified user a message
-app.post("/send/user/message", (reg, res) => {
-
-});
-
 //~~~~~~~~~~~~~~~~~~Misc requests
 
 //return the current user based on client's cookie
@@ -485,6 +460,7 @@ app.get('/get/username/', (req, res) => {
 });
 
 /*    FUNCTIONS   */
+// uses cookies to authorize the logins
 function authorize(req, res, next) {
     //update to a more secure salting and hashing method with session keys
     if (req.cookies.login.username != undefined)
@@ -493,15 +469,8 @@ function authorize(req, res, next) {
         res.end('unauthorized');
 }
 
-//function for hashing the password when creating user
-function hashPass(password){
-
-}
-
 /*    RUNTIME    */
-
 //pages to serve depending on path
-//app.use('/accountCreation.html');
 app.use('/chats.html', authorize);
 app.use('/home.html', authorize);
 app.use('/', express.static("public_html"));
